9613cbdaa3eb6cddad62fba43869a3b7e3181cc5
tamir-blink <tamir.ifrach@blinkops.com>
2025-03-17 18:26:56 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -63,39 +63,35 @@ return issuer({
       issuer: "https://dev-87536712.okta.com/oauth2/default",
     }),
   },
-      theme: {
-        title: "myAuth",
-        primary: "#0051c3",
-        favicon: "https://workers.cloudflare.com//favicon.ico",
-        logo: {
-          dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
-          light:
-            "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
-        },
-      },
-      success: async (ctx, value) => {
-        return ctx.subject("user", {
-          id: await getOrCreateUser(env, value.email),
-        });
-      },
-    }).fetch(request, env, ctx);
+  theme: {
+    title: "myAuth",
+    primary: "#0051c3",
+    favicon: "https://workers.cloudflare.com//favicon.ico",
+    logo: {
+      dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+      light:
+        "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+    },
   },
+  success: async (ctx, value) => {
+    return ctx.subject("user", {
+      id: await getOrCreateUser(env, value.email),
+    });
+  },
+}).fetch(request, env, ctx);
+},
 } satisfies ExportedHandler<Env>;
 
 async function getOrCreateUser(env: Env, email: string): Promise<string> {
   const result = await env.AUTH_DB.prepare(
     
-		INSERT INTO user (email)
-		VALUES (?)
-		ON CONFLICT (email) DO UPDATE SET email = email
-		RETURNING id;
-		,
+    "INSERT INTO user (email) VALUES (?) ON CONFLICT (email) DO UPDATE SET email = email RETURNING id;",
   )
     .bind(email)
     .first<{ id: string }>();
   if (!result) {
-    throw new Error(Unable to process user: ${email});
+    throw new Error(`Unable to process user: ${email}`);
   }
-  console.log(Found or created user ${result.id} with email ${email});
+  console.log(`Found or created user ${result.id} with email ${email}`);
   return result.id;
 }
